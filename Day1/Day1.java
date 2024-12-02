acb29d49c5f9ba88a13e0c3bb204fb189e349610
TristanKalvoda <kalvo007@morris.umn.edu>
2024-12-02 15:36:56 -0600

    Day 1 Part 2

--- Day1/Day1.java
+++ Day1/Day1.java
@@ -1,13 +1,13 @@
 import java.io.*;
 import java.util.Scanner;
 import java.util.Arrays;
-import java.lang.math;
+import java.lang.Math;
 
 public class Day1 {
     public static void main(String[] args) {
         int lines = 0;
         try {
-            File inputFile = new File("day1Input.txt");
+            File inputFile = new File("Day1/day1Input.txt");
             Scanner lineCounter = new Scanner(inputFile);
             while (lineCounter.hasNextLine()) {
                 lineCounter.nextLine();
@@ -19,10 +19,6 @@ public class Day1 {
             int[] arr2 = new int[lines];
 
             Scanner myReader = new Scanner(inputFile);
-            // while (myReader.hasNextLine()) {
-            // System.out.println("arr1:" + myReader.nextInt());
-            // System.out.println("arr2:" + myReader.nextInt());
-            // }
             for (int i = 0; i < lines; i++) {
                 arr1[i] = myReader.nextInt();
                 arr2[i] = myReader.nextInt();
@@ -32,26 +28,33 @@ public class Day1 {
             // arr1: first column's numbers
             // arr2: second column's numbers
 
-            Arrays.sort(arr1);
-            Arrays.sort(arr2);
-            //Arrays are now Sorted
-
-            getDifferences(arr1,arr2);
-            System.out.println(getDifferences(arr1, arr2));
-
-            
-            // System.out.println(Arrays.toString(arr1));
-            // System.out.println(Arrays.toString(arr2));
+            System.out.println("Difference:"+getDifferences(arr1, arr2));
+            System.out.println("Similarity Score:"+getSimilarityScore(arr1, arr2));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public static int getDifferences(int[] arr1, int[] arr2){
+    public static int getDifferences(int[] arr1, int[] arr2) {
         int sum = 0;
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
         for(int i = 0; i<arr1.length; i++) {
             sum += Math.abs((arr1[i]-arr2[i]));
         }
         return sum;
+    }
+
+    public static int getSimilarityScore(int[] arr1, int[] arr2){
+        int similarityScore = 0;
+        for(int i = 0; i<arr1.length; i++) {
+            int tmp = 0;
+            for(int j = 0; j<arr1.length; j++) {
+                if (arr1[i] == arr2[j]) tmp++;
+            }
+            similarityScore += tmp*arr1[i];
+        }
+        return similarityScore;
+        // Score:23472342 too low
     }
 }
